d5c1dc0778a65d5b71176720009cf9d7d85b4bda
anWin <anne920606@gmail.com>
2022-04-21 18:02:14 +0800

    check parentheses

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,20 +1,83 @@
 #include "BigNumber.h"
+#include "vector"
+#include "string"
 using namespace std;
 
+bool checkParentheses(const string& input);
+
+bool isSetInt(const string& input);
+bool isSetDec(const string& input);
 
 int main() {
 	string input;
+	vector<string> varList;
+	//BigNumber result;
+	bool notError;
 	while (getline(cin, input)) {
-		if (input[0] == NULL)
+		notError = 1;
+		// input error
+		if (input[0] == NULL) {
 			cout << "Input should not be empty." << endl;
-		if (input[0] == 'S') {
-			// Set Integer A = 123
-			// Set Decimal B = 12.34
+			continue;
 		}
 
+		if (!checkParentheses(input)) {
+			cout << "Parentheses Error" << endl;
+			continue;
+		}
 
-		cout << input;
-		BigNumber (input);
+		// Set Integer/Decimal A
+		if (isSetInt(input)) {
+			cout << "Set Integer." << endl;
+		}
+		else if (isSetDec(input)) {
+			cout << "Set Decimal." << endl;
+		}
+
+		if (notError) {
+			//result = input;
+			//cout << result;
+		} else {
+			cout << "Error-in-variable" << endl;
+		}
 	}
 }
+
+
+bool checkParentheses(const string& input) {
+	size_t left = count(input.begin(), input.end(), '(');
+	size_t right = count(input.begin(), input.end(), ')');
+	if (left != right) 
+		return false;
+	size_t found = input.find("()");
+	if (found != std::string::npos)
+		return false;
+	return true;
+}
+
+
+bool isSetInt(const string & input) {
+	string strSet = "Set Integer";
+	int i = 0;
+	if (input.size() < strSet.size()) return false;
+	for (char s : strSet) {
+		if (s != input[i]) 
+			return false;
+		i++;
+	}
+	return true;
+}
+
+bool isSetDec(const string& input) {
+	string strSet = "Set Decimal";
+	int i = 0;
+	if (input.size() < strSet.size()) return false;
+	for (char s : strSet) {
+		if (s != input[i])
+			return false;
+		i++;
+	}
+	return true;
+}
+
 
