3ba8eaca5838405667ec3e4dc7c758543b2ac0df
RinRainbow <105558966+RinRainbow@users.noreply.github.com>
2022-05-18 16:53:02 +0800

    each .cpp files

---
+++ Project2/Project2/Guard.cpp
@@ -0,0 +1,35 @@
+#include "Chess.h"
+
+Guard::Guard() {
+    pos = std::make_pair(0, 0);
+}
+
+Guard::Guard(const Guard& guard) {
+    pos = guard.pos;
+}
+
+Guard::Guard(const int& X, const int& Y) {
+    pos = std::make_pair(X, Y);
+}
+
+Guard::Guard(const std::pair<int, int>& Pos) {
+    pos = Pos;
+}
+
+Guard::~Guard() {
+    
+}
+
+Guard& Guard::operator= (const Guard& guard) {
+    pos = guard.pos;
+    return *this;
+}
+
+Guard& Guard::operator= (const std::pair<int, int>& Pos) {
+    pos = Pos;
+    return *this;
+}
+
+void Guard::getSuggestion() {
+    
+}
