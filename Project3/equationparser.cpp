20fa5bba365edebb3d75d209864041eac1898e9d
an <50503333+zhian66@users.noreply.github.com>
2022-06-20 15:00:51 +0800

    Add files via upload

--- Project3/equationparser.cpp
+++ Project3/equationparser.cpp
@@ -1,90 +1,452 @@
 #include "equationparser.h"
 
-EquationParser::EquationParser()
-{
+EquationParser::EquationParser() {
+	this->input = "";
+	this->leftRange = -10;
+	this->rightRange = 10;
+	this->precision = 0.1;
+}
+
+EquationParser::EquationParser(string input, double range, double precision) {
+	this->leftRange = -range;
+	this->rightRange = range;
+	this->precision = precision;
+	this->input = input;
+}
+
+void EquationParser::setString(string input) {
+	this->input = input;
+}
+
+void EquationParser::checkInput(const string& input) {
+	string vaildStr = "0123456789()+-*/=!.^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+	int bracket = 0;
+	int leftBracketIdx = -1, rightBracketIdx = -1;
+
+	// check number of equal sign
+	size_t countEqual = count(input.begin(), input.end(), '=');
+	if (countEqual != 1)
+		throw exception("Invalid input about '='");
+
+	for (int i = 0; i < input.size(); i++) {
+		char c = input[i];
+
+		// check if input exists invaild character
+		size_t found = vaildStr.find(c);
+		if (found == string::npos)
+			throw exception("Invalid input about illegal character");
+
+		// check if the parentheses brackets valid
+		if (c == '(') {
+			bracket++;
+			leftBracketIdx = i;
+		} else if (c == ')') {
+			bracket--;
+			rightBracketIdx = i;
+			if (leftBracketIdx + 1 == rightBracketIdx)
+				throw exception("Parenthese bracket is empty");
+		}
+		if (bracket < 0)
+			throw exception("Imcomplete parentheses brackets");
+	}
+
+	if (bracket != 0) {
+		throw exception("Imcomplete parentheses brackets");
+	}
 
 }
 
+void EquationParser::checkLeftVariable(const string& input) {
+	size_t foundEqualIdx = input.find('=');
+	if (foundEqualIdx == string::npos)
+		throw exception("Invalid input about '='");
+	for (int i = 0; i < foundEqualIdx; i++) {
+		if (isOper(input[i]))
+			throw exception("Invalid input about left equation");
+	}
+}
+
+void EquationParser::setRange(double range) {
+	leftRange = -range;
+	rightRange = range;
+}
+
+void EquationParser::setPrecision(double precision) {
+	this->precision = precision;
+}
+
+void EquationParser::getVector(vector<double>& x_, vector<double>& y_) {
+	xVec.clear();
+	yVec.clear();
+	// check input 
+	string input = this->input;
+	checkInput(input);
+	checkLeftVariable(input);
+
+	// input processing
+	// remove space
+	input.erase(std::remove(input.begin(), input.end(), ' '), input.end());
+
+	// left is y
+	if (input[0] == 'y') {
+		size_t found = input.find("=");
+		string rightStr = input.substr(found + 1);
+		replaceVar(rightStr);
+		parser(rightStr);
+
+		double y;
+		for (double x = leftRange; x <= rightRange; x += precision) {
+			xVec.push_back(x);
+			y = calculate(x);
+			yVec.push_back(y);
+		}
+	}
+	// left is x
+	else if (input[0] == 'x') {
+
+	}
+	// Is variable
+	else {
+		addVar(input);
+	}
+	x_ = xVec;
+	y_ = yVec;
+}
+
+bool EquationParser::isOper(char s) {
+	string operStr = "+-*/=!^#~";
+	size_t found = operStr.find(s);
+	if (found == string::npos) {
+		return false;
+	}
+	return true;
+}
+
+bool EquationParser::stopVar(char s) {
+	string operStr = "+-*/=!^()";
+	size_t found = operStr.find(s);
+	if (found == string::npos) {
+		return false;
+	}
+	return true;
+}
+
+bool EquationParser::isNum(string input) {
+	for (auto c : input) {
+		if ((c < '0' || c > '9') && c != '.') {
+			return false;
+		}
+	}
+	return true;
+}
+
+void EquationParser::addVar(string input) {
+	const string reservedWord[4] = { "x", "sin", "cos", "tan" };
+	string var;
+	size_t foundEqual = input.find('=');
+
+	var = input.substr(0, foundEqual);
+	string num = input.substr(foundEqual + 1);
+	string varTmp = "";
+
+	for (int i = 0; i < num.size(); i++) {
+		bool isAlphabet =
+			(num[i] <= 'z' && num[i] >= 'a') ||
+			(num[i] <= 'Z' && num[i] >= 'A');
+
+		if (isAlphabet) {
+			varTmp += num[i];
+		} else {
+			if (varTmp == "")
+				continue;
+
+			bool isResevedW = false;
+			for (auto r : reservedWord) {
+				if (varTmp == r) {
+					isResevedW = true;
+					break;
+				}
+			}
+			if (isResevedW)
+				continue;
+
+			if (varList.size() == 0) {
+				Error = Undefined_Variable;
+				throw exception("Exist undefined variable");
+			}
+			for (auto varPair : varList) {
+				size_t foundVar = varTmp.find(varPair.first);
+				if (foundVar == string::npos) {
+					Error = Undefined_Variable;
+					throw exception("Exist undefined variable");
+				} else {
+					num.erase(foundVar, varPair.first.size());
+					num.insert(foundVar, "(" + varPair.second + ")");
+				}
+			}
+			varTmp = "";
+		}
+	}
+
+	bool exist = 0;
+	for (auto& v : varList) {
+		if (v.first == var) {
+			exist = 1;
+			Error = Variable_Already_Exists;
+			throw exception("Variable already exist");
+		}
+	}
+	if (!exist) {
+		varList.push_back(make_pair(var, num));
+	}
+}
+
+void EquationParser::replaceVar(string& input) {
+	const string reservedWord[4] = { "x", "sin", "cos", "tan" };
+	string varTmp = "";
+	int varIdx = -1;
+	bool isVar = false; // if encouter alphabet, then is a variable
+
+	for (int i = 0; i < input.size(); i++) {
+		if (input[i] <= 'z' && input[i] >= 'a' ||
+			input[i] <= 'Z' && input[i] >= 'A') {
+			isVar = true;
+			varIdx = i;
+			varTmp += input[i];
+
+		} else if (isVar && (input[i] <= '9' && input[i] >= '0')) {
+			varTmp += input[i];
+		}
+		if (stopVar(input[i]) || i == input.size() - 1) {
+			if (!isVar)
+				continue;
+
+
+			bool isResevedW = false;
+			for (auto r : reservedWord) {
+				if (varTmp == r) {
+					isResevedW = true;
+					break;
+				}
+			}
+			if (!isResevedW) {
+				bool foundVar = false;
+				for (auto v : varList) {
+					if (varTmp == v.first) {
+						foundVar = true;
+						input.erase(varIdx, varTmp.size());
+						input.insert(varIdx, "(" + v.second + ")");
+						break;
+					}
+				}
+				if (!foundVar) {
+					throw exception("Undefined variable");
+				}
+			}
+
+			varTmp = "";
+			isVar = false;
+		}
+	}
+}
 
 int EquationParser::priority(char op) {
-    switch (op) {
-    case '+': case '-': return 1;
-    case '*': case '/': return 2;
-    case '^': case '!': case '#': case '~': return 3;
-    default:            return 0;
-    }
+	switch (op) {
+	case '+': case '-': return 1;
+	case '*': case '/': return 2;
+	case '#': case '~': return 3;
+	case '^': return 3;
+	case 's': case 'c': case 't': return 4;
+	default:            return 0;
+	}
 }
 
-int EquationParser::calculate(const string& input) {
-    stack<char> temp;
-    bool first = true;
-    bool preNum = false;
-    int sPtr = 0, len = 0;
-    string str = "";
+void EquationParser::parser(const string& input) {
+	postfix.clear();
+	stack<char> temp;
+	bool firstNum = true;
+	bool preNum = false;
+	bool preRightBracket = false;
+	int sPtr = 0, len = 0;
+	string str = "";
 
-    for (int i = 0; i < input.size(); i++) {
-        char c = input[i];
-        if (c == ' ') continue;
+	// sin, cos, tan
+	int funcAmount = 3;
+	string function[3] = { "sin(", "cos(", "tan(" };
 
-        if ((c >= '0' && c <= '9') || c == '.') {
-            if (first) {
-                postfix.push_back(str);
-                sPtr = i;
-            }
-            first = false;
-            preNum = true;
-            postfix[postfix.size()-1].push_back(c);
-        } else {
-            first = true;
-            str = "";
-        }
+	for (int i = 0; i < input.size(); i++) {
+		char c = input[i];
+		if (c == ' ') continue;
+		if (c == 'x') {
+			postfix.push_back("x");
+			preNum = true;
+			continue;
+		}
 
-        if (priority(c) > 0) {
-            if (c == '-' && !preNum) {
-                c = '~';
-            } else if (c == '+' && !preNum) {
-                c = '#';
-            }
-            if (!temp.empty() && temp.top() != '(') {
-                if (priority(temp.top()) >= priority(c)) {	// c <= top
-                    do {
-                        string tmp(1, temp.top());
-                        postfix.push_back(tmp);
-                        temp.pop();
-                    } while (!temp.empty() && priority(temp.top()) >= priority(c));
-                    temp.push(c);
-                    preNum = false;
-                } else {									// c > top
-                    temp.push(c);
-                    preNum = false;
-                }
-            } else {
-                temp.push(c);
-            }
-            preNum = false;
-        }
+		// Parse number
+		if ((c >= '0' && c <= '9') || c == '.') {
+			if (firstNum) {
+				postfix.push_back(str);
+				sPtr = i;
+			}
+			firstNum = false;
+			preNum = true;
+			preRightBracket = false;
+			postfix[postfix.size() - 1].push_back(c);
+			continue;
+		} else {
+			firstNum = true;
+			str = "";
+		}
 
-        if (c == '(') {
-            temp.push(c);
-            preNum = false;
-        }
-        if (c == ')') {
-            while (!temp.empty()) {
-                if (temp.top() == '(') {
-                    temp.pop();
-                    break;
-                } else {
-                    string tmp(1, temp.top());
-                    postfix.push_back(tmp);
-                    temp.pop();
-                }
-            }
-        }
-    }
-    while (!temp.empty()) {
-        string tmp(1, temp.top());
-        postfix.push_back(tmp);
-        temp.pop();
-    }
-    return sum();
+		// Parse the sin, cos, and tan
+		if (c <= 'z' && c >= 'a') {
+			bool isFunc = false;
+			for (int j = 0; j < funcAmount; j++) {
+				for (int s = 0; s < function[j].size(); s++) {
+					if (input[i + s] != function[j][s]) {
+						isFunc = false;
+						break;
+					}
+					isFunc = true;
+				}
+				if (isFunc) {
+					c = function[j][0];
+					i += function[j].size() - 2;
+					break;
+				}
+			}
+			if (!isFunc)
+				throw exception("function error");
+		}
+
+		// Parse operator
+		if (priority(c) > 0) {
+			if (!preNum) {
+				if (c == '-')
+					c = '~';
+				else if (c == '+')
+					c = '#';
+				else if (!preRightBracket && !(c <= 'z' && c >= 'a'))
+					throw exception("Operator error");
+			}
+			if (!temp.empty() && temp.top() != '(') {
+				if (priority(temp.top()) >= priority(c)) {
+					do {
+						string tmp(1, temp.top());
+						postfix.push_back(tmp);
+						temp.pop();
+					} while (!temp.empty() && priority(temp.top()) >= priority(c));
+					temp.push(c);
+					preNum = false;
+				} else {
+					temp.push(c);
+					preNum = false;
+				}
+			} else {
+				temp.push(c);
+			}
+			preNum = false;
+			preRightBracket = false;
+		}
+
+		if (c == '(') {
+			temp.push(c);
+			preNum = false;
+			preRightBracket = false;
+		} else if (c == ')') {
+			while (!temp.empty()) {
+				if (temp.top() == '(') {
+					temp.pop();
+					break;
+				} else {
+					string tmp(1, temp.top());
+					postfix.push_back(tmp);
+					temp.pop();
+				}
+			}
+			preRightBracket = true;
+		}
+	}
+	while (!temp.empty()) {
+		string tmp(1, temp.top());
+		postfix.push_back(tmp);
+		temp.pop();
+	}
 }
+
+double EquationParser::calculate(double x) {
+	stack<double> numStack;
+	double num;
+
+	for (int i = 0; i < postfix.size(); i++) {
+		string str = postfix[i];
+		if (str == "x") {
+			numStack.push(x);
+		} else if (isNum(str)) {
+			std::stringstream ss;
+			ss << str;
+			ss >> num;
+			numStack.push(num);
+		} else if (str == "+") {
+			num = numStack.top();
+			numStack.pop();
+			num = num + numStack.top();
+			numStack.pop();
+			numStack.push(num);
+		} else if (str == "-") {
+			num = numStack.top();
+			numStack.pop();
+			num = numStack.top() - num;
+			numStack.pop();
+			numStack.push(num);
+		} else if (str == "*") {
+			num = numStack.top();
+			numStack.pop();
+			num = numStack.top() * num;
+			numStack.pop();
+			numStack.push(num);
+		} else if (str == "/") {
+			num = numStack.top();
+			numStack.pop();
+
+			if (num == 0) {
+				throw exception("Divide by zero");
+				break;
+			}
+			num = numStack.top() / num;
+			numStack.pop();
+			numStack.push(num);
+		} else if (str == "^") {
+			num = numStack.top();
+			numStack.pop();
+			num = pow(numStack.top(), num);
+			numStack.pop();
+			numStack.push(num);
+		} else if (str == "~") {
+			num = -numStack.top();
+			numStack.pop();
+			numStack.push(num);
+		} else if (str == "#") {
+		} else if (str == "s") {
+			num = sin(numStack.top());
+			numStack.pop();
+			numStack.push(num);
+		} else if (str == "c") {
+			num = cos(numStack.top());
+			numStack.pop();
+			numStack.push(num);
+		} else if (str == "t") {
+			num = tan(numStack.top());
+			numStack.pop();
+			numStack.push(num);
+		}
+	}
+
+	return numStack.top();
+}
+
+
+
+
--- Project3/equationparser.h
+++ Project3/equationparser.h
@@ -1,10 +1,13 @@
 #ifndef EQUATIONPARSER_H
 #define EQUATIONPARSER_H
 
+#include <iostream>
 #include <vector>
 #include <string>
 #include <stack>
-#include <QDebug>
+#include <exception>
+#include <sstream>
+
 using namespace std;
 
 class EquationParser
@@ -12,14 +15,43 @@ class EquationParser
 
 public:
     EquationParser();
+    EquationParser(string input, double range = 10, double precision = 10);
+    void setString(string input);
+    void setRange(double range);
+    void setPrecision(double precision);
+    void getVector(vector<double> &x_, vector<double> &y_);
 
 
 private:
+    //QVector<double>xVec, yVec;
+    vector<double> xVec, yVec;
     vector<string> postfix;
-    vector<pair<string, double>> varList;
+    vector<pair<string, string>> varList;
+    string input;
+    double leftRange, rightRange, precision;
 
+    int Error;
+    enum ERROR_CODE {
+        No_Error = 0,
+        Invaild_Input = 1,
+        Invaild_Variable = 2,
+        Variable_Already_Exists = 3,
+        Undefined_Variable = 4
+    };
+
+    void checkInput(const string &input);
+    void checkLeftVariable(const string &input);
+
+    bool isOper(char s);
+    bool stopVar(char s);
+    bool isNum(string input);
     int priority(char op);
-    int calculate(const string& input);
+
+    void addVar(string input);
+    void replaceVar(string &input);
+    void parser(const string& input);
+    double calculate(double x);
+
 };
 
 
